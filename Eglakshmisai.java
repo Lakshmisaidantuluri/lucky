776926425c13f1d94e867f860e89381fbf5b46aa
Lakshmisaidantuluri <92712793+Lakshmisaidantuluri@users.noreply.github.com>
2021-12-02 10:47:35 +0530

    Add files via upload

---
+++ Eglakshmisai.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author student
+ */
+public class Eglakshmisai 
+{
+  public static void main(String args[])
+  {
+      System.out.println("hi");
+  }
+}
+      
+  
+        
+        
+        
+
